176d81297dab6b54e1ca8d8015307e994667b26b
crazy6901 <harshkothari17@gmail.com>
2020-12-13 01:40:39 +0530

    Updated version 2.0|

--- SIS/__init__.py
+++ SIS/__init__.py
@@ -6,8 +6,8 @@ import os
 app = Flask(__name__)
 
 app.config['SECRET_KEY']=os.environ.get('API_KEY')
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(' SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(' SQLALCHEMY_DATABASE_URI')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 db = SQLAlchemy(app)
 
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from SIS import app,create_app
+from SIS import app
 
-app = create_app()
+#app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
