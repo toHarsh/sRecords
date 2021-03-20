de41f941431d74fb9d3e1019f2b1a1e267293ae1
toHarsh <harsh.kothari-coep@bvucoep.edu.in>
2021-03-21 03:33:28 +0530

    database added 1.4

--- SIS/__init__.py
+++ SIS/__init__.py
@@ -5,7 +5,8 @@ import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY']=os.environ.get('API_KEY')
+# app.config['SECRET_KEY']=os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY']="8a62b083c268dfcf91b523a0b5eb9f08"
 app.config['SQLALCHEMY_DATABASE_URL'] = os.environ.get('DATABASE_URL')
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
